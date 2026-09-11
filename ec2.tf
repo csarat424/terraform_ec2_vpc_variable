2c390e2cf1b905740651b5b6bb0222da7bcc9c39
csarat424 <csarat424@gmail.com>
2026-09-11 07:45:16 -0400

    commented ec2 and private subnets

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "pub_server" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  availability_zone           = var.az[0]
-  associate_public_ip_address = "true"
+# resource "aws_instance" "pub_server" {
+#   ami                         = var.ami
+#   instance_type               = var.instance_type
+#   key_name                    = var.key_name
+#   availability_zone           = var.az[0]
+#   associate_public_ip_address = "true"
 
 
-  tags = {
-    Name = "terraform_ec2"
-  }
+#   tags = {
+#     Name = "terraform_ec2"
+#   }
 
-}
+# }
 
--- rta.tf
+++ rta.tf
@@ -15,17 +15,17 @@ resource "aws_route_table_association" "pub_rta_3" {
 }
 
 # Private RTA
-resource "aws_route_table_association" "private_rta_1" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_rt.id
-}
+# resource "aws_route_table_association" "private_rta_1" {
+#   subnet_id      = aws_subnet.private_subnet_1.id
+#   route_table_id = aws_route_table.private_rt.id
+# }
 
-resource "aws_route_table_association" "private_rta_2" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_rt.id
-}
+# resource "aws_route_table_association" "private_rta_2" {
+#   subnet_id      = aws_subnet.private_subnet_2.id
+#   route_table_id = aws_route_table.private_rt.id
+# }
 
-resource "aws_route_table_association" "private_rta_3" {
-  subnet_id      = aws_subnet.private_subnet_3.id
-  route_table_id = aws_route_table.private_rt.id
-}
+# resource "aws_route_table_association" "private_rta_3" {
+#   subnet_id      = aws_subnet.private_subnet_3.id
+#   route_table_id = aws_route_table.private_rt.id
+# }
